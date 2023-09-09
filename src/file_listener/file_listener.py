3f9cf9de7b5cf3b4ca0c7b662eb3541d8c45e1d6
Tetro <itsofirk@gmail.com>
2023-09-09 17:13:36 +0300

    initial commit

--- src/file_listener/file_listener.py
+++ src/file_listener/file_listener.py
@@ -4,3 +4,6 @@ from .kafka_producer import KafkaProducer
 class FileListener:
     def __init__(self, directory, kafka_addresses, topic_prefix):
         self.kafka = KafkaProducer(kafka_addresses, topic_prefix)
+
+    def listen(self):
+        pass
