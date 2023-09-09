bea511a20824f230400bb65fe0fe927955b9690c
Tetro <itsofirk@gmail.com>
2023-09-09 15:14:21 +0300

    added a basic logger

--- src/app.py
+++ src/app.py
@@ -1,3 +1,4 @@
+import logging
 import argparse
 from file_listener import FileListener
 
@@ -12,11 +13,11 @@ parser.add_argument('-k', '--kafka', metavar='hostname:port', help='kafka addres
 parser.add_argument('-t', '--topic', default='fl', metavar='prefix',
                     help='specify a prefix for the kafka topics: prefix-jpeg, prefix-pdf (default: fl)', )
 
+logger = logging.getLogger('file_listener')
+
 
 if __name__ == '__main__':
-    # Print hello message
+    logger.info("File Listener initializing!")
     args = parser.parse_args()
-    # Parse arguments
     # Initiate FileListener
     # Start listener
-    ...
--- src/file_listener/__init__.py
+++ src/file_listener/__init__.py
@@ -1,1 +1,6 @@
+import logging
 from .file_listener import FileListener
+
+
+logging.basicConfig(level=logging.INFO)
+logging.getLogger(__name__)
