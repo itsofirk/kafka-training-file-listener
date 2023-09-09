dad46970f753b3a11f7e0672ebbae00ad277d97f
Tetro <itsofirk@gmail.com>
2023-09-09 15:26:55 +0300

    added validators to the directory and kafka_host arguments

--- src/arguments.py
+++ src/arguments.py
@@ -1,4 +1,24 @@
+from file_listener.exceptions import FileListenerException
+import os
+
 import argparse
+
+
+def directory(_arg):
+    if not os.path.exists(_arg):
+        raise FileListenerException("Provided path does not seem to exist.")
+    if not os.path.isdir(_arg):
+        raise FileListenerException("Provided path does not seem to be a directory.")
+    return _arg
+
+
+def kafka_host(_arg):
+    parts = _arg.split(':')
+    if not len(parts) == 2 or ' ' in _arg:
+        raise FileListenerException("Provided kafka address is not in the correct format (host:port).")
+    if not parts[1].isdigit() or 1 < int(parts[1]) < 25565:
+        raise FileListenerException("Kafka port must be a number.")
+
 
 parser = argparse.ArgumentParser(
     prog='File Listener',
@@ -6,7 +26,8 @@ parser = argparse.ArgumentParser(
                 'extension-specific topic'
 )
 
-parser.add_argument('-d', '--directory', help='The directory to listen to', required=True)
-parser.add_argument('-k', '--kafka', metavar='hostname:port', help='kafka address to send messages into', required=True)
+parser.add_argument('-d', '--directory', type=directory, help='The directory to listen to', required=True)
+parser.add_argument('-k', '--kafka', type=kafka_host, metavar='hostname:port',
+                    help='kafka address to send messages into', required=True)
 parser.add_argument('-t', '--topic', default='fl', metavar='prefix',
                     help='specify a prefix for the kafka topics: prefix-jpeg, prefix-pdf (default: fl)', )
